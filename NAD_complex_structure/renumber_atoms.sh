840b1ca054d31f883f692972d10b5cb29f117017
Ekaterina Osipova <osipovarev@gmail.com>
2025-01-24 10:36:17 -0500

    backed up changes from Fri Jan 24 10:36:17 EST 2025 in the project /Users/osipova/Documents/LabDocs/Brood_parasites_analysis/NAD_complex_structure

--- NAD_complex_structure/renumber_atoms.sh
+++
@@ -1,6 +0,0 @@
-awk 'BEGIN {atom_count=1} 
-     /^ATOM|^HETATM/ {
-         printf "%s%5d%s\n", substr($0, 1, 6), atom_count, substr($0, 12);
-         atom_count++;
-     }
-     !/^ATOM|^HETATM/ { print }'  $1
